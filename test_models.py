4b1e96e433e5caf464befe326ecce3b2f4af50b0
SeldomseenSchweig <jamesschweig@gmail.com>
2022-07-28 09:27:37 -0700

    all the tests work but the user id column is left blan and throws and error when I try to add it

--- test_models.py
+++ test_models.py
@@ -55,17 +55,22 @@ class UerModelTestCase(TestCase):
        
 
     def test_post_database(self):
-        user1 = User.query.get(1)
-        post1 = Post(title="bleh", content="my first blag post", user_id=user1.id)
+        user2 = User(first_name="Test", last_name="testerino", image_url='https://cdn.pixabay.com/photo/2022/03/16/01/23/bird-7071408__340.jpg' )
+        user1 = User(first_name="John", last_name="jacob jingelheimerschmidht", image_url='https://cdn.pixabay.com/photo/2022/03/16/01/23/bird-7071408__340.jpg')
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.commit()
+        # user1 = User.query.get(1)
+        post1 = Post(title="bleh", content="my first blag post", id=1)
         db.session.add(post1)
         db.session.commit()
-        user2 = User.query.get(2)
-        post2 = Post(title="bleh", content="my second blag post", user_id =user2.id)
+        # user2 = User.query.get(2)
+        post2 = Post(title="bleh", content="my second blag post", id =2)
         db.session.add(post2)
         db.session.commit()
         first_post = Post.query.get(1)
         second_post = Post.query.get(2)
 
-        self.assertEqual(first_post.user_id,1)
+        self.assertEqual(first_post.content,'my first blag post')
         self.assertEqual(second_post.title,"bleh")
 
